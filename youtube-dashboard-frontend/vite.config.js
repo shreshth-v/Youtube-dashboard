8fea9c80e5babb84ff0e186eb8e3a22d9d26b4fc
Shreshth <shreshth100vishwa@gmail.com>
2025-06-15 12:23:00 +0530

    Added vercel configuration

--- youtube-dashboard-frontend/vite.config.js
+++ youtube-dashboard-frontend/vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-],
-})
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+  },
+  preview: {
+    port: 4173,
+  },
+});
